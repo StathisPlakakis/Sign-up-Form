4729b80a06351f5fc412df295395b087f1745dcf
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-03-21 21:23:45 +0200

    Add validation to password & password confirmation

--- functions.js
+++ functions.js
@@ -1,10 +1,36 @@
 const inputt = document.querySelector("#phone");
+
 inputt.addEventListener("input", (e) => {
     const value = e.target.value.trim();
-    if (isNaN(value) || value.length < 7 || value.length > 12) {
+    if (isNaN(value) || value.length < 10 || value.length > 10) {
         e.target.classList.add('invalid');
+        
     } else {
         e.target.classList.remove('invalid');
     }
     
 })
+
+const pass1 = document.querySelector("#Password");
+const pass2 = document.querySelector("#CPassword");
+
+pass1.addEventListener("input", (e) => {
+    const length = e.target.value.length;
+    if (length < 8) {
+        e.target.classList.add('invalid')
+    } else {
+        e.target.classList.remove('invalid');
+    }
+})
+
+pass2.addEventListener("input", (e) => {
+    const conf = e.target.value;
+    if (conf !== pass1.value) {
+        e.target.classList.add("invalid")
+    } else {
+        e.target.classList.remove('invalid');
+    }
+})
+
+
+
